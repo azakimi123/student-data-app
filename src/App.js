a2b99c05730344ac969ebe8ece8de560cb6d451f
azakimi123 <aaron.zakimi@gmail.com>
2020-10-10 10:34:02 -0600

    components being exported

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import Auth from './Components/Auth/Auth';
+import Dashboard from './Components/Dashboard/Dashboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Auth />
+      <Dashboard />
     </div>
   );
 }
