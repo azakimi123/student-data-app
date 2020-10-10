dfb16149f1caab9b538125f1729898ec225872c2
azakimi123 <aaron.zakimi@gmail.com>
2020-10-10 10:04:16 -0600

    component set up

---
+++ src/Components/Classes/Classes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Classes() {
+  return (
+    <div>
+      <h1>Classes Page</h1>
+    </div>
+  )
+}
+
+export default Classes;
---
+++ src/Components/Goals/Goals.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Goals() {
+  return(
+    <div>
+      <h1>Goals Page</h1> 
+    </div>
+  )
+}
+
+export default Goals;
